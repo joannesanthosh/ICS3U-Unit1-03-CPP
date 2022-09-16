c450aee0a3576908853b7e71eec42a7d842b71f8
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-09-16 14:42:38 +0000

    init

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Joanne Santhosh All rights reserved
+//
+// Created by: Joanne Santhosh
+// Created on: Sept 2022
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\nDone.";
+}
